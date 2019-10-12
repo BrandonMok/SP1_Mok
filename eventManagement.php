19bac9c26f9273157ff9890a42b649e42d5a7e96
BrandonMok <brandonmok88@yahoo.com>
2019-10-12 15:28:20 -0400

    Started on eventManagement edit event

--- eventManagement.php
+++ eventManagement.php
@@ -27,6 +27,49 @@
                         if(managementEditDeleteCheck()){
                             if($_GET["action"] == "edit"){
                                 // EDIT
+                                $id = $_GET["id"];
+                                $event = $db->getEvent($id)[0]; // get the selected event
+
+                                // Store original values to compare to on POST
+                                // Don't want to keep querying same object when doing post logic
+                                $originalValues = array(
+                                    "name" => $event->getName(),
+                                    "datestart" => $event->getDateStart(),
+                                    "dateend" => $event->getDateEnd(), 
+                                    "numberallowed" => $event->getNumberAllowed(),
+                                    "venue" => $event->getVenue()
+                                );
+                                $originalValues = json_encode($originalValues);
+
+                                // EDIT VENUE SECTION
+                                echo "<h2 class='section-heading'>Edit Venue</h2>";
+
+                                $eventEditTable = "<div id='account-form-container'>
+                                                    <form id='user-edit-form' name='user-edit-form' action='./eventManagement.php?id={$event->getIdEvent()}&action=edit' method='POST'>
+                                                        <div id='user-edit-labels'>
+                                                            <label>ID</label>
+                                                            <label>Name</label>
+                                                            <label>Date Start</label>
+                                                            <label>Date End</label>
+                                                            <label>Number Allowed</label>
+                                                            <label>Venue</label>   
+                                                        </div>
+                                                        <div id='user-edit-inputs'>
+                                                            <input type='text' name='id' value='{$event->getIdEvent()}' readonly='readonly'><br/>
+                                                            <input type='text' name='name' value='{$event->getName()}'><br/>
+                                                            <input type='text' name='datestart' value='{$event->getDateStart()}'><br/>
+                                                            <input type='text' name='dateend' value='{$event->getDateEnd()}'><br/>
+                                                            <input type='text' name='numberallowed' value='{$event->getNumberAllowed()}'><br/>
+                                                            <input type='text' name='venue' value='{$event->getVenue()}'><br/>
+                                                        </div><br/>
+                                                        <input type='hidden' name='originalValues' value='{$originalValues}'>";
+                                                    
+                                $eventEditTable .= "<input name='submit' id='submit-btn' type='submit' value='Submit'/></form></div>";
+                                echo $eventEditTable;
+
+
+
+
 
                             }
                             else if($_GET["action"] == "delete"){
@@ -42,7 +85,7 @@
                             // Add 
                             if($_GET["action"] == "add"){
 
-
+                                // validate date using validation functions
 
 
                             }
@@ -72,6 +115,28 @@
                 // REDIRECT: User not logged in
                 redirect("login");
             }
+
+
+
+
+
+
+
+
+
+
+
+            /** -------------------- POST LOGIC --------------------*/
+            if($_SERVER["REQUEST_METHOD"] == "POST"){
+                if(isset($_GET["action"]) && !empty($_GET["action"])){
+                    if($_GET["action"] == "edit"){
+
+                    }
+                    else if($_GET["action"] == "add") {
+                        
+                    }
+                }
+            }// end if POST
         ?>
     </body>
 </html>
